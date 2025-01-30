07c726f5ce4a3523087fc4a197f85f5d0f9c773f
hadi <hadi@itobuz.com>
2025-01-30 13:03:14 +0530

    feat: deleting sessions while seeding

--- backend/seed/index.js
+++ backend/seed/index.js
@@ -4,11 +4,12 @@ import genrateNote from "./note.js";
 import genrateUser from "./user.js";
 import User from "../src/models/user.js";
 import Note from "../src/models/note.js";
+import Session from "../src/models/session.js";
 const main= async ()=>{
         await mongoose.connect(process.env.CONNECTION_STRING)
         await User.deleteMany({});
         await Note.deleteMany({});
-        
+        await Session.deleteMany({});
 
         await genrateUser(10);
         await genrateNote(50);
