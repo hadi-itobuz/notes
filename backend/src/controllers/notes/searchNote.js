9799d294986d7be8137e8f17923560bbfe1a55c6
hadi <hadi@itobuz.com>
2025-01-30 11:09:41 +0530

    feat: error handelling in  searchNote

--- backend/src/controllers/notes/searchNote.js
+++ backend/src/controllers/notes/searchNote.js
@@ -4,19 +4,24 @@ const searchNote = async (req, res) => {
     try {
         const { userId } = req.body;
         const searchText=req.query.searchText;
-        // const user = await User.findById(userId);
-        const notes = await Note.find({
-            userId,
-            '$or': [
-                { 'title': { '$regex': `${searchText}`, '$options': 'i' } },
-                { 'body': { '$regex': `${searchText}`, '$options': 'i' } }
-            ]
+        if(searchText){
+            const notes = await Note.find({
+                userId,
+                '$or': [
+                    { 'title': { '$regex': `${searchText}`, '$options': 'i' } },
+                    { 'body': { '$regex': `${searchText}`, '$options': 'i' } }
+                ]
+            })
+            res.status(200).send({
+                success: true,
+                message: `${notes.length} notes found`,
+                notes
+            });
+        }else res.status(500).send({
+            success: false,
+            message: "Unable to search notes"
         })
-        res.status(200).send({
-            success: true,
-            message: `${notes.length} notes found`,
-            notes
-        });
+        
     } catch (err) {
         res.status(500).send({
             success: false,
