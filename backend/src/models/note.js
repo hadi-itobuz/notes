18b00035d5c99b47d0429070394f4d7ec441f65a
hadi <hadi@itobuz.com>
2025-01-30 15:49:31 +0530

    feat: added fileName field to Note schema

--- backend/src/models/note.js
+++ backend/src/models/note.js
@@ -17,6 +17,9 @@ const notesSchema = new mongoose.Schema({
         type: Date,
         required: true,
         default: Date.now
+    },
+    file: {
+        type: String
     }
 })
 const Note = mongoose.model("Note", notesSchema);
