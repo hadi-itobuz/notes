47f792a7bda2d5c5014bdd792f96591462bf475a
hadi <hadi@itobuz.com>
2025-01-31 18:04:19 +0530

    feat: fianlized note routes

--- backend/src/routes/noteRoute.js
+++ backend/src/routes/noteRoute.js
@@ -1,33 +1,38 @@
 import express from "express"
+//create
 import addNote from "../controllers/notes/addNote.js";
+//read
+import getNotes from "../controllers/notes/getNotes.js";//seaching sorting pagenation
 import getAll from "../controllers/notes/getAllNotes.js";
 import getById from "../controllers/notes/getNoteById.js";
-import editNote from "../controllers/notes/editNote.js";
-import deleteById from "../controllers/notes/deleteNoteById.js";
 import getSorted from "../controllers/notes/getSortedNotes.js";
-import searchNote from "../controllers/notes/searchNote.js";
+import searchNote from "../controllers/notes/searchNote.js";ß
+import getFile from "../controllers/notes/getFile.js";
+//update
+import editNote from "../controllers/notes/editNote.js";
+import {attachFile,upload} from "../controllers/notes/attachFile.js";
+//delete
+import deleteById from "../controllers/notes/deleteNoteById.js";
+//validation
 import { noteSchema, validateData } from "../middleware/verifyCredentials.js";
+//middleware
 import { verifyAccessToken } from "../middleware/verifyToken.js";
 import isLoggedIn from "../middleware/isLoggedIn.js";
-import {attachFile,upload} from "../controllers/notes/attachFile.js";
-import getNotes from "../controllers/notes/getNotes.js";
-import getFile from "../controllers/notes/getFile.js";
+
 const noteRoute = express.Router();
-
-noteRoute.get('/', verifyAccessToken,isLoggedIn,getNotes);//this route can get,search,sort and paginate notes
-//pageNumber(>0), notePerPage(>0), sortBy(title,body,createdOn), order(+1 or -1), searchText
-
-//operations wrt to user id
+//Create
 noteRoute.post('/add', verifyAccessToken, isLoggedIn, validateData(noteSchema), addNote);
+//R
+noteRoute.get('/', verifyAccessToken,isLoggedIn,getNotes);//this route can get,search,sort and paginate notesSS//pageNumber(>0), notePerPage(>0), sortBy(title,body,createdOn), order(+1 or -1), searchText
 noteRoute.get('/getAll', verifyAccessToken, isLoggedIn, getAll); //get all notes pertaining to a user
 noteRoute.get('/search', verifyAccessToken, isLoggedIn, searchNote);
 noteRoute.post('/getSorted', verifyAccessToken, isLoggedIn, getSorted)
-
-//operations wrt to note id
 noteRoute.get('/getId/:id', verifyAccessToken, isLoggedIn, getById);
-noteRoute.delete('/deleteId/:id', verifyAccessToken, isLoggedIn, deleteById);
+noteRoute.get('/getFile/:id',getFile);//File
+//U
 noteRoute.put('/edit/:id', verifyAccessToken, isLoggedIn, editNote);
-noteRoute.put('/upload/:id',verifyAccessToken,isLoggedIn,upload.single('filename'),attachFile);
-noteRoute.get('/getFile/:id',getFile);
+noteRoute.put('/upload/:id',verifyAccessToken,isLoggedIn,upload.single('filename'),attachFile);//File:Multer
+//D
+noteRoute.delete('/deleteId/:id', verifyAccessToken, isLoggedIn, deleteById);
 
 export default noteRoute;
