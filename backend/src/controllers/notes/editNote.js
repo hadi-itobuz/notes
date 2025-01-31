fba9fee5a0cec4c19ed69d5f01ad04ab115ab01b
hadi <hadi@itobuz.com>
2025-01-31 15:11:59 +0530

    fix:checking for duplicate while editing

--- backend/src/controllers/notes/editNote.js
+++ backend/src/controllers/notes/editNote.js
@@ -2,18 +2,26 @@ import Note from "../../models/note.js";
 const editNote = async (req, res) => {
     const id = req.params.id;
     const { title, body } = req.body;
+    const userId= req.body.userId;
     try {
         const note = await Note.findOne({ userId, title });
-        if (title !== note.title && note._id != id) {
+        if (!note) {
             await Note.findByIdAndUpdate(id, { title, body }, { new: true });
             res.status(201).send({
                 success: true,
                 message: "Note edited"
             })
-        }else{
+        }else if(note.id===id){
+            await Note.findByIdAndUpdate(id, { title, body }, { new: true });
+            res.status(201).send({
+                success: true,
+                message: "Note edited"
+            })
+        } 
+        else {
             res.status(400).send({
-                success:false,
-                message:"Unable to delete: duplicate title"
+                success: false,
+                message: "Unable to delete: duplicate title"
             })
         }
 
