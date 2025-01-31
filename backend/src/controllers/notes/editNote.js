f6699e03882920dd44c4af68820ae8e86dc2a57e
hadi <hadi@itobuz.com>
2025-01-31 14:52:38 +0530

    fix: added duplicate title check to editNote

--- backend/src/controllers/notes/editNote.js
+++ backend/src/controllers/notes/editNote.js
@@ -3,11 +3,20 @@ const editNote = async (req, res) => {
     const id = req.params.id;
     const { title, body } = req.body;
     try {
-        await Note.findByIdAndUpdate(id, { title, body }, { new: true });
-        res.status(201).send({
-            success: true,
-            message: "Note edited"
-        })
+        const note = await Note.findOne({ userId, title });
+        if (title !== note.title && note._id != id) {
+            await Note.findByIdAndUpdate(id, { title, body }, { new: true });
+            res.status(201).send({
+                success: true,
+                message: "Note edited"
+            })
+        }else{
+            res.status(400).send({
+                success:false,
+                message:"Unable to delete: duplicate title"
+            })
+        }
+
     } catch (err) {
         console.log('err :>> ', err);
         res.status(400).send({
