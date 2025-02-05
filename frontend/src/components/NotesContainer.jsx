1fcfabcb331bf47f6ed517e94fbd97766e5dd445
hadi <hadi@itobuz.com>
2025-02-05 17:24:26 +0530

    feat: implemented genral route to get notes while searching, sorting and pagenation

--- frontend/src/components/NotesContainer.jsx
+++ frontend/src/components/NotesContainer.jsx
@@ -6,27 +6,31 @@ import instance from "../../axiosConfig";
 
 const NotesContainer = () => {
     const [notes, setNotes] = useState(null);
+    const [searchOptions, setSearchOptions] = useState({
+        pageNumber: 1,
+        notePerPage: 4,
+        order: 1,
+        searchText: ""
+    })
     const updateNotes = async () => {
-        console.log('Updating :>> ', );
-        const res = await instance.get('/notes/getAll')
-        console.log('res.data.notes :>> ', res.data.notes);
+        console.log('Updating :>> ',);
+        // const res = await instance.get('/notes/getAll')
+        const res= await instance.post('/notes/',searchOptions)
+        console.log('res.data.notes :>> ', res);
         setNotes(res.data.notes);
     }
     useEffect(() => {
         setNotes(null);
-        console.log('Mount :>> ');
         return () => {
-            console.log('dismount :>> ');
             updateNotes()
         }
-    },[]);
+    }, [searchOptions]);
 
-    const [searchField, setSearchField] = useState('');
     return (
 
         <div className="container p-3">
             <h1>NOTES...</h1>
-            <NotesContainerHeader searchField={searchField} setSearchField={setSearchField} />
+            <NotesContainerHeader searchOptions={searchOptions} setSearchOptions={setSearchOptions} />
             <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
                 {(notes) ? notes.map(note => (<Note key={note._id} note={note} />)) : <h1>Loading.....</h1>}
             </div>
@@ -34,3 +38,19 @@ const NotesContainer = () => {
     )
 }
 export default NotesContainer;
+
+
+
+
+
+// let config = {
+//   data : data
+// };
+
+// axios.request(config)
+// .then((response) => {
+//   console.log(JSON.stringify(response.data));
+// })
+// .catch((error) => {
+//   console.log(error);
+// });
