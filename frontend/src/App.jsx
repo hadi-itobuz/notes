febd9929d1e2508f97b4383ef793a1b72981d31e
hadi <hadi@itobuz.com>
2025-02-06 15:40:17 +0530

    fix: on reload updating state by making api call

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,37 @@ import {
   Route,
   // Link
 } from "react-router-dom";
-import {useState } from 'react';
+import { useState } from 'react';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
 import ProtectedRoute from './components/ProtectedRoute';
-const checkToken=()=>{
-  const token=5;
+import axios from 'axios';
+const isLoggedIn =async () => {
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: 'http://localhost:3000/user/getAccessToken',
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('refreshToken')}` 
+    }
+  };
+  axios.request(config)
+    .then((response) => {
+      localStorage.setItem('accessToken',response.data.accessToken);
+      return true;
+    })
+    .catch((error) => {
+      console.log(error);
+      return false;
+    });
+
 }
 function App() {
-  const [login,setLogin]=useState(false);
+  const [login, setLogin] = useState(isLoggedIn());
   return (
     <>
- 
+
       <Router>
         <Routes>
           <Route path="/" element={<Login setLogin={setLogin} />} />
