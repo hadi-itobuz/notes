259d2fac1e11e29b42dd29fa143e74799d93a915
hadi <hadi@itobuz.com>
2025-02-06 12:49:44 +0530

    feat: pagenation

--- frontend/src/components/NotesContainer.jsx
+++ frontend/src/components/NotesContainer.jsx
@@ -1,38 +1,43 @@
 import { useState } from "react";
 import Note from "./Note";
 import NotesContainerHeader from "./NoteContainerHeader";
-import { useEffect } from "react";
+import { useEffect,useRef } from "react";
 import instance from "../../axiosConfig";
+import NotePagenaton from "./NotePageination";
 
 const NotesContainer = () => {
     const [notes, setNotes] = useState(null);
     const [searchOptions, setSearchOptions] = useState({
         pageNumber: 1,
-        notePerPage: 20,
+        notePerPage: 6,
         sortBy: "title",
         order: 1,
         searchText: ""
     })
-    const updateNotes = async () => {
-        const res = await instance.post('/notes/', searchOptions)
-        setNotes(res.data.notes);
-    }
+    const previousState=useRef(searchOptions)
     useEffect(() => {
+        const updateNotes = async () => {
+            const res = await instance.post('/notes/', searchOptions)
+            if(res.data.notes.length>0){
+                previousState.current=searchOptions;//saving valid states
+                setNotes(res.data.notes);
+            }else{
+                setSearchOptions(previousState.current);
+            }
+        
+        }
         updateNotes();
-        // setNotes(null);
-        // return () => {
-        //     updateNotes()
-        // }
     }, [searchOptions]);
 
     return (
 
         <div className="container p-3">
             <h1>NOTES...</h1>
-            <NotesContainerHeader searchOptions={searchOptions} setSearchOptions={setSearchOptions} />
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+            <NotesContainerHeader setSearchOptions={setSearchOptions} />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-2 py-5">
                 {(notes) ? notes.map(note => (<Note key={note._id} note={note} />)) : <h1>Loading.....</h1>}
             </div>
+            <NotePagenaton setSearchOptions={setSearchOptions} searchOptions={searchOptions}/>
         </div>
     )
 }
