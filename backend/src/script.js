2cf7d2f22aaad17e8c9a7f88eb8d086c70a2024d
hadi <hadi@itobuz.com>
2025-02-14 15:00:37 +0530

    feat: rate limit in backend

--- backend/src/script.js
+++ backend/src/script.js
@@ -3,15 +3,24 @@ import 'dotenv/config';
 import connectToDb from "./config/connectToDb.js";
 import noteRoute from "./routes/noteRoute.js";
 import userRoute from "./routes/userRoute.js";
+import { rateLimit } from "express-rate-limit";
 import cors from "cors";
+const limiter = rateLimit({
+    windowMs: 5*60 * 1000,
+    limit: 10,
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
 const app = express()
 app.use(express.json());
 app.use(cors());
+// app.use(limiter);
 
 connectToDb();//connectong to mongo db data base
 
 app.use('/notes', noteRoute);
-app.use('/user',userRoute)
+app.use('/user', limiter, userRoute)
 app.listen(process.env.PORT, () => {
     console.log(`Listening at port: http://localhost:${process.env.PORT}/`);
 })
