1cf8b7abe627afcf795c7dd6b20ba08e012fee13
hadi <hadi@itobuz.com>
2025-02-14 10:24:33 +0530

    fix: visibility of note modal is boolean

--- frontend/src/components/Note/NoteModal.jsx
+++ frontend/src/components/Note/NoteModal.jsx
@@ -9,7 +9,7 @@ const NoteModal = ({ setVisibility, type }) => {
                         <h3 className="text-lg font-semibold text-white">
                             {type.header} Note
                         </h3>
-                        <button onClick={() => { setVisibility('hidden') }} type="button" className="text-gray-400 bg-transparent rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center hover:bg-gray-600 hover:text-white" data-modal-toggle="crud-modal">
+                        <button onClick={() => { setVisibility(false) }} type="button" className="text-gray-400 bg-transparent rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center hover:bg-gray-600 hover:text-white" data-modal-toggle="crud-modal">
                             <svg className="w-3 h-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                             </svg>
