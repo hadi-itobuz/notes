836bcafa049724398aa58c7d9fedf5c93ca09045
hadi <hadi@itobuz.com>
2025-02-14 12:32:38 +0530

    feat: if field is password ability to toggle visibility

--- frontend/src/components/Form/FormField.jsx
+++ frontend/src/components/Form/FormField.jsx
@@ -2,23 +2,43 @@ import {
     Input,
     Typography,
 } from "@material-tailwind/react";
+import { useState } from "react";
 import PropTypes from "prop-types"
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+
 const FormField = ({ fieldObj, register }) => {
+    const [type, setType] = useState(fieldObj.type);
+    const handleToggle = () => {
+        if (type === 'password') {
+            setType('text')
+        } else {
+            setType('password')
+        }
+    }
+
     return (
         <>
             <Typography variant="h6" color="white" className="-mb-3">
                 {fieldObj.name}
             </Typography>
-            <Input
-                type={fieldObj.type}
-                defaultValue={fieldObj.val} {...register(`${fieldObj.name}`, { required: true })}
-                size="lg"
-                placeholder={fieldObj.val}
-                className=" !border-t-blue-gray-200 focus:!border-t-gray-900 text-white p-3"
-                labelProps={{
-                    className: "before:content-none after:content-none",
-                }}
-            />
+            <div className="flex">
+
+                <Input
+                    type={type}
+                    defaultValue={fieldObj.val} {...register(`${fieldObj.name}`, { required: true })}
+                    size="lg"
+                    placeholder={fieldObj.val}
+                    className=" !border-t-blue-gray-200 focus:!border-t-gray-900 text-white p-3"
+                    labelProps={{
+                        className: "before:content-none after:content-none",
+                    }}
+                />
+                {(fieldObj.type === 'password') && <span className=" z-50 -ml-8 flex justify-around items-center" onClick={handleToggle}>
+                    {(type==='password')?<FaEye color={"white"} />:<FaEyeSlash color={"white"} />}
+                </span>}
+            </div>
+
         </>
     )
 }
